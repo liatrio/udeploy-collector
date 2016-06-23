31b8858e6537f9637a200e35eb4b65269d1feb11
Benjamin Stein <ben@liatrio.com>
2016-06-23 15:58:47 -0700

    Add constructor

--- models/component.js
+++ models/component.js
@@ -6,17 +6,26 @@ var schemas = require('./../schemas');
 var _ = require('lodash');
 
 var Component = function(data) {
-    this.data = this.sanitize()
+    this.data = this.prepData(data);
+    this.data = this.sanitize(data);
 };
 
 Component.prototype.data  = {};
 
-
+Component.prototype.prepData = function(data) {
+    this.data.name = data.component.name;
+    this.data.version = data.version.name;
+    this.data.environmentId = data.environment.environmentId;
+    this.data.environmentName = data.environment.name;
+    this.data.deployed = data.compliancy.desiredCount == data.compliancy.missingCount;
+    this.data.asOfDate = data.date;
+    return this.data;
+};
 Component.prototype.sanitize = function(data) {
     data = data || {};
     var schema = schemas.component;
     return _.pick(_.defaults(data,schema), _.keys(schema));
-}
+};
 
 Component.findById = function() {
 
