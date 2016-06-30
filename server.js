b70c7c0ea949c43857866ff03bd34a540827e016
Benjamin Stein <ben@liatrio.com>
2016-06-29 23:02:54 -0700

    will move to cron later on

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 var ucd = require('./ucd');
+var utility = require('./utility')
 
 
 http.createServer(function (request, response) {
@@ -22,12 +23,26 @@ http.createServer(function (request, response) {
     //    response.end(environmentResponseObj);
     //    return response;
     //});
-    return ucd.getComponentsInEnvironment().then(function(components) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var componentResponseObj = JSON.stringify({components: components });
-        response.end(componentResponseObj);
-        return response;
+    //return ucd.getComponentsInEnvironment().then(function(components) {
+    //    response.writeHead(200, {"Content-Type": "text/plain"});
+    //    var componentResponseObj = JSON.stringify({components: components });
+    //    response.end(componentResponseObj);
+    //    return response;
+    //});
+    //return ucd.getEnvironmentResourceStatusData({application: null, environment: null}).then(function(components) {
+    //    response.writeHead(200, {"Content-Type": "text/plain"});
+    //    var componentResponseObj = JSON.stringify({components: components });
+    //    response.end(componentResponseObj);
+    //    return response;
+    //});
+
+    return ucd.getApplications().then(function(applications) {
+            return utility.successfulRequest(response,{ applications: applications}).then(function(applications) {
+
+            });
     });
+
+
 }).listen(8000);
 
 console.log("Server running at localhost:8000/");
