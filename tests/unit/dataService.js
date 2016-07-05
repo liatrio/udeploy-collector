7513a87b8753437f39a2d467d800b4487874dc6e
Benjamin Stein <ben@liatrio.com>
2016-07-05 15:20:01 -0700

    dataservice unit test

---
+++ tests/unit/dataService.js
@@ -0,0 +1,12 @@
+"use strict";
+var assert = require('assert'),
+    DB = require("../../db"),
+    Promise = require('promise');
+
+describe('Collector tests', function() {
+    describe('', function() {
+        it('Test collect and save', function() {
+
+        });
+    });
+});
