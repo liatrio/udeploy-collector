b7af452ac4f921cd0cfc57ec1b61c361caff5060
Benjamin Stein <ben@liatrio.com>
2016-07-05 15:20:15 -0700

    collector skeleton unit test

---
+++ tests/unit/collector.js
@@ -0,0 +1,14 @@
+"use strict";
+var assert = require('assert'),
+    DB = require("../../db"),
+    Promise = require('promise'),
+    Collector = require('../../collector');
+
+describe('Collector tests', function() {
+    describe('', function() {
+       it('Test collect and save', function() {
+
+       });
+    });
+});
+
