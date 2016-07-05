bff307bfc700493f736a2c49568d2151c2ad45db
Benjamin Stein <ben@liatrio.com>
2016-07-05 14:45:24 -0700

    constants

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 "use strict";
 
 module.exports = Object.freeze({
-    MISSING_FIELDS: "Missing Fields: "
+    MISSING_FIELDS: "Missing Fields: ",
+    COLLECTOR_NAME: "UDeploy",
+    COLLECTOR_TYPE: "Deployment"
 });
