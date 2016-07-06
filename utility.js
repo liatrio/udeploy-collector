460ff2b83a59869d1d9955776317a12c489595c6
Benjamin Stein <ben@liatrio.com>
2016-07-06 10:54:32 -0700

    make utility class use static methods

--- utility.js
+++ utility.js
@@ -1,19 +1,20 @@
 'use strict';
 var constants = require("./constants");
 
-var Utility = function() {};
+var Utility = {};
 
-Utility.prototype.missingFieldsError = function(response) {
+Utility.missingFieldsError = function(response) {
     response.writeHead(400, {"Content-Type": "text/plain"});
     var componentResponseObj = JSON.stringify({message: constants.MISSING_FIELDS });
     response.end(componentResponseObj);
     return response;
 };
-Utility.prototype.successfulRequest = function(response,messageObj) {
+Utility.successfulRequest = function(response,messageObj) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     var responseObj = JSON.stringify(messageObj);
     response.end(responseObj);
     return response;
-}
+};
 
-module.exports = new Utility();
+
+module.exports =  Utility;
