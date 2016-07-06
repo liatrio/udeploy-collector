2bbfc8a2bf6335a91d08e90590c335f95fbcfd84
Benjamin Stein <ben@liatrio.com>
2016-07-06 14:54:29 -0700

    getCollector returns actual collector object with id

--- collector.js
+++ collector.js
@@ -1,23 +1,23 @@
 "use strict";
 var Promise = require('promise'),
+    moment = require('moment'),
     DataService = require('./ucdDataService'),
     Db = require('./db'),
     constants = require('./constants'),
     collectorId = null,
     dataService = null,
+    collectorObj = require('./models/collector'),
     Collector = function() {
         dataService = new DataService();
     };
 
 Collector.prototype.collectAndSave = function() {
-    return getCollector().then(getCollectorItems).then(updateCollectorItems).then(collect).then(function(data) {
-        console.log(data);
-        return transform(data);
-    }).then(function(data) {
-        console.log(data);
-        return save(data);
-    });
-
+    return getCollector()
+        .then(getCollectorItems)
+        .then(updateCollectorItems)
+        .then(collect)
+        .then(transform)
+        .then(save);
 };
 
 var getCollector = function() {
@@ -31,36 +31,45 @@ var getCollector = function() {
     collector.online = true;
     collector.lastExecuted = moment.now();
     var db = new Db('collectors');
-    return db.upsertByName( new Collector(collector) ).then(function(result) {
+    return db.upsertByName( new collectorObj(collector).data ).then(function(result) {
+        return db.findOne({name: collector.name})
+    }).then(function(result){
         collectorId = result._id;
+        console.log("RESULT",result);
         return Promise.resolve(result);
     }).catch(function(err) {
-        console.log(err);
         console.log("unable to get collector id");
         return Promise.reject(err);
     })
 };
 
-var getCollectorItems = function() {
+var getCollectorItems = function(collector) {
+    console.log(collector);
     return dataService.getCollectorItems();
-};
+}
 
 var updateCollectorItems = function(collectorItems) {
+    console.log("Collector Items", collectorItems);
     //get all collector items from database
     //compare to collectorItems in ucd
     //update database list
+    return Promise.resolve(collectorItems);
 };
 
 var collect = function(){
-    return new DataService.collect();
+    return Promise.resolve();
+    //return new DataService.collect();
 };
 var transform = function() {
+
   return Promise.resolve();
 };
 
 var save = function(data) {
-    var db = new Db(data.document);
-    return db.insertOne(data);
+    console.log("saving")
+    //var db = new Db(data.document);
+    return Promise.resolve();
+    //return db.insertOne(data);
 
 };
 module.exports = Collector;
