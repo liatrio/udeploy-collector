84ca44c3a2940875fc8d55a881ff170e87ca0615
Benjamin Stein <ben@liatrio.com>
2016-07-12 15:57:36 -0700

    get rid of unnecessary utility file

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 "use strict";
 
 var http = require('http'),
-    utility = require('./utility'),
     Collector = require('./collector');
 
 http.createServer(function (request, response) {
     return new Collector().collectAndSave().then(function(result) {
-       return utility.successfulRequest(response,result);
+       return successfulRequest(response,result);
     });
 }).listen(8000);
+
+var missingFieldsError = function(response) {
+    response.writeHead(400, {"Content-Type": "text/plain"});
+    var componentResponseObj = JSON.stringify({message: constants.MISSING_FIELDS });
+    response.end(componentResponseObj);
+    return response;
+};
+var successfulRequest = function(response,messageObj) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var responseObj = JSON.stringify(messageObj);
+    response.end(responseObj);
+    return response;
+};
 
 console.log("Server running at localhost:8000/");
