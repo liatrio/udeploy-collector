747e5142d1f9868757ec704cab9f05e11e1d90fc
Benjamin Stein <ben@liatrio.com>
2016-07-14 09:42:21 -0700

    THEIA-48 switch http requests to cron job

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 "use strict";
 
-var http = require('http'),
+var CronJob = require('cron').CronJob,
     Collector = require('./collector');
 
-http.createServer(function (request, response) {
-    return new Collector().collectAndSave().then(function(result) {
-       return successfulRequest(response,result);
-    });
-}).listen(8000);
+    new CronJob('10 * * * *', function() {
+        console.log("Start collecting");
+        new Collector().collectAndSave();
 
-var missingFieldsError = function(response) {
-    response.writeHead(400, {"Content-Type": "text/plain"});
-    var componentResponseObj = JSON.stringify({message: constants.MISSING_FIELDS });
-    response.end(componentResponseObj);
-    return response;
-};
-var successfulRequest = function(response,messageObj) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var responseObj = JSON.stringify(messageObj);
-    response.end(responseObj);
-    return response;
-};
-
-console.log("Server running at localhost:8000/");
+    }, null, true, 'America/Los_Angeles');
